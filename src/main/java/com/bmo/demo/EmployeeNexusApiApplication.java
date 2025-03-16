19e655974ef5ea21d2545ef7df3fb33c0b619f89
Jteche <aarif.diwan@gmail.com>
2025-03-16 14:37:24 -0400

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/bmo/demo/EmployeeNexusApiApplication.java
@@ -0,0 +1,13 @@
+package com.bmo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeNexusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeNexusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmo/demo/EmployeeNexusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeNexusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
