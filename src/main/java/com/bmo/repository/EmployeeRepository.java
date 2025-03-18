0a223424db3ee2caf509afc19c20e7c7324cecf9
Jteche <aarif.diwan@gmail.com>
2025-03-18 12:50:56 -0400

    docs : add vscode launcher code cleanup

--- src/main/java/com/bmo/repository/EmployeeRepository.java
+++ src/main/java/com/bmo/repository/EmployeeRepository.java
@@ -2,12 +2,11 @@ package com.bmo.repository;
 
 import com.bmo.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 /**
  * JPA Repository interface for Employee entities.
- * Extends JpaRepository to inherit basic CRUD operations and pagination support.
+ * Extends JpaRepository to inherit basic CRUD operations and pagination
+ * support.
  * No custom methods needed as basic JPA functionality covers all requirements.
  */
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
